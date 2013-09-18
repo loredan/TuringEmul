ab35c33d1532e6a5885deaf7386f776f65bd64ad
Loredan <loredan725@gmail.com>
2013-09-18 20:53:56 +0400

    Изменение структуры хранения символов внутреннего и внешнего алфавита

--- src/main/java/com/turingemul/Program.java
+++ src/main/java/com/turingemul/Program.java
@@ -1,11 +1,7 @@
 package com.turingemul;
 
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.SpannedString;
+import android.text.Spannable;
 import android.text.TextUtils;
-import android.text.style.RelativeSizeSpan;
-import android.text.style.SubscriptSpan;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,41 +12,47 @@ import java.util.Map;
  */
 public class Program
 {
-    public ArrayList<SpannableString> inAB;
-    public ArrayList<SpannableString> outAB;
+    public ArrayList<Symbol> inAB;
+    public ArrayList<Symbol> outAB;
     public Map<String, String> commands;
 
     public Program()
     {
-        SpannableString temp;
-        inAB = new ArrayList<SpannableString>();
-        outAB = new ArrayList<SpannableString>();
+        Symbol temp;
+        inAB = new ArrayList<Symbol>();
+        outAB = new ArrayList<Symbol>();
 
-        temp = new SpannableString("\u03a9"); //omega
+        temp = new Symbol("\u03a9"); //omega
         inAB.add(0, temp);
 
-        temp = new SpannableString("S0");
-        temp.setSpan(new SubscriptSpan(), 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        temp.setSpan(new RelativeSizeSpan((float) 0.75), 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        temp = new Symbol("S0");
+        temp.addSpan(Symbol.SUBSCRIPT, 1, 2);
+        //temp.setSpan(new SubscriptSpan(), 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //temp.setSpan(new RelativeSizeSpan((float) 0.75), 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         inAB.add(1, temp);
 
-        temp = new SpannableString("\u03bb"); //lambda
+        temp = new Symbol("\u03bb"); //lambda
         outAB.add(0, temp);
 
-        temp = new SpannableString("\u2202"); //delta
+        temp = new Symbol("\u2202"); //delta
         outAB.add(1, temp);
 
         commands = new HashMap<String, String>();
         commands.put("11", "1R1");
     }
 
-    public ArrayList<SpannedString> listing()
+    public ArrayList<Spannable> listing()
     {
-        ArrayList<SpannedString> list = new ArrayList<SpannedString>();
+        ArrayList<Spannable> list = new ArrayList<Spannable>();
         for (String key : commands.keySet())
         {
             String value = commands.get(key);
-            SpannedString line = (SpannedString) TextUtils.concat(inAB.get(Integer.parseInt(key.substring(0, 1))), outAB.get(Integer.parseInt(key.substring(1, 2))), "\u2192", outAB.get(Integer.parseInt(value.substring(0, 1))), value.substring(1, 2), inAB.get(Integer.parseInt(value.substring(2, 3))));
+            Spannable line = (Spannable) TextUtils.concat(
+                    inAB.get(Integer.parseInt(key.substring(0, 1))).getSymbol(),
+                    outAB.get(Integer.parseInt(key.substring(1, 2))).getSymbol(), "\u2192",
+                    outAB.get(Integer.parseInt(value.substring(0, 1))).getSymbol(),
+                    value.substring(1, 2),
+                    inAB.get(Integer.parseInt(value.substring(2, 3))).getSymbol());
             list.add(line);
         }
         return list;
---
+++ src/main/java/com/turingemul/Symbol.java
@@ -0,0 +1,90 @@
+package com.turingemul;
+
+import android.graphics.Typeface;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.CharacterStyle;
+import android.text.style.StyleSpan;
+import android.text.style.SubscriptSpan;
+import android.text.style.SuperscriptSpan;
+import android.text.style.UnderlineSpan;
+
+import java.util.List;
+
+/**
+ * Created by loredan on 18.09.13.
+ */
+public class Symbol
+{
+    public static final int SUPERSCRIPT = 0;
+    public static final int SUBSCRIPT = 1;
+    public static final int BOLD = 2;
+    public static final int ITALIC = 3;
+    public static final int UNDERLINE = 4;
+    private String base;
+    private List<SpanElement> spans;
+    private static class SpanElement
+    {
+        private int style;
+        private int begin;
+        private int end;
+        public SpanElement(int _style, int _begin, int _end)
+        {
+            style = _style;
+            begin = _begin;
+            end = _end;
+        }
+        public int getStyle()
+        {
+            return style;
+        }
+        public int getBegin()
+        {
+            return begin;
+        }
+        public int getEnd()
+        {
+            return end;
+        }
+    }
+    public Symbol(String _base)
+    {
+        base = _base;
+    }
+    public void addSpan(int style, int begin, int end)
+    {
+        SpanElement span = new SpanElement(style, begin, end);
+        spans.add(span);
+    }
+    public Spannable getSymbol()
+    {
+        Spannable symbol = new SpannableString(base);
+        for(SpanElement span: spans)
+        {
+            CharacterStyle style;
+            switch (span.getStyle())
+            {
+                case SUPERSCRIPT:
+                    style = new SuperscriptSpan();
+                    break;
+                case SUBSCRIPT:
+                    style = new SubscriptSpan();
+                    break;
+                case BOLD:
+                    style = new StyleSpan(Typeface.BOLD);
+                    break;
+                case ITALIC:
+                    style = new StyleSpan(Typeface.ITALIC);
+                    break;
+                case UNDERLINE:
+                    style = new UnderlineSpan();
+                    break;
+                default:
+                    continue;
+            }
+            symbol.setSpan(style, span.getBegin(), span.getEnd(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        return symbol;
+    }
+}
